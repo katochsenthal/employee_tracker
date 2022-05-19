0fb9ca1d138609986093cc7d82789490c9c870f3
katochsenthal <katoch.senthal@gmail.com>
2022-05-18 20:01:44 -0400

    installed packages

---
+++ server.js
@@ -0,0 +1,8 @@
+const mysql = require("mysql2");
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "test",
+});
