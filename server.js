20ac5dfe3bffdc4e3c44702563a20622b612f060
katochsenthal <katoch.senthal@gmail.com>
2022-05-19 22:00:45 -0400

    user input prompts

--- server.js
+++ server.js
@@ -21,3 +21,59 @@ const connection = mysql.createConnection({
   database: "employee_db",
   password: "katoch13",
 });
+
+const promptMenu = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menu",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "Exit",
+        ],
+      },
+    ])
+    .then((userAnswer) => {
+      switch (userAnswer.menu) {
+        case "View all departments":
+          console.log("Departments");
+          break;
+
+        case "View all roles":
+          console.log("Roles");
+          break;
+
+        case "View all employees":
+          console.log("Employees");
+          break;
+
+        case "add a department":
+          console.log("Add a department");
+          break;
+
+        case "add a role":
+          console.log("add a role");
+          break;
+
+        case "add a employee":
+          console.log("add a employee");
+          break;
+
+        case "update an employee role":
+          console.log("update a employee role");
+          break;
+
+        default:
+          process.exit();
+      }
+    });
+};
+
+promptMenu();
