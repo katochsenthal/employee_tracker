30f1244d3a673d1e92d133ea2da9cfd0428bb930
katochsenthal <katoch.senthal@gmail.com>
2022-05-19 21:57:03 -0400

    db connection established

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const express = require("express");
 const mysql = require("mysql2");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("express working!");
+});
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // create the connection to database
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  database: "test",
+  database: "employee_db",
+  password: "katoch13",
 });
